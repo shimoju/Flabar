d367c06702c1b307190b559530b957f19d443d2f
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2013-11-10 19:19:00 +0900

    WriteViewControllerクラスを作成しView Controllerのカスタムクラスとして設定

---
+++ Flabar/FlabarWriteViewController.h
@@ -0,0 +1,13 @@
+//
+//  FlabarWriteViewController.h
+//  Flabar
+//
+//  Created by Hiroshi Shimoju on 2013/11/10.
+//  Copyright (c) 2013年 dbsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlabarWriteViewController : UIViewController
+
+@end
---
+++ Flabar/FlabarWriteViewController.m
@@ -0,0 +1,38 @@
+//
+//  FlabarWriteViewController.m
+//  Flabar
+//
+//  Created by Hiroshi Shimoju on 2013/11/10.
+//  Copyright (c) 2013年 dbsh. All rights reserved.
+//
+
+#import "FlabarWriteViewController.h"
+
+@interface FlabarWriteViewController ()
+
+@end
+
+@implementation FlabarWriteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
