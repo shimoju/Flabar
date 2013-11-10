828d85756616440b7c7fea4d2c1e417741d38c39
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2013-11-10 15:27:06 +0900

    Initial Commit

---
+++ Flabar/FlabarAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FlabarAppDelegate.h
+//  Flabar
+//
+//  Created by Hiroshi Shimoju on 2013/11/10.
+//  Copyright (c) 2013年 dbsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlabarAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
